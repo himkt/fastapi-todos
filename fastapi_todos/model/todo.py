e5d7a2b0d7dd94a111ccea146c5b58147b94d0d2
himkt <himkt@cookpad.com>
2021-10-06 23:38:20 +0900

    Make id and created_at optional

--- fastapi_todos/model/todo.py
+++ fastapi_todos/model/todo.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from sqlalchemy import Column, DateTime
 from sqlalchemy.sql import func
@@ -6,11 +7,11 @@ from sqlmodel import Field, SQLModel
 
 
 class Todo(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     finished: bool = False
     # https://github.com/tiangolo/sqlmodel/issues/21
-    created_at: datetime = Field(
+    created_at: Optional[datetime] = Field(
         default=None,
         sa_column=(Column(DateTime(timezone=True), server_default=func.now())),
     )
